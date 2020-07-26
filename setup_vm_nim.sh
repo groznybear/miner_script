031950aa19dae86efa7723ccb56f766b37ce3147
GroznyBear <groznybear@gmail.com>
2020-07-26 20:57:01 +0300

    no message

--- setup_vm_nim.sh
+++ setup_vm_nim.sh
@@ -14,5 +14,3 @@ for i in `seq 1 4`;
 do
     sudo timeout 1000m ./miner --wallet-address="$wallet1" --pool=$pool_address1 --deviceLabel=$miner_id
 done
-
-/bin/bash -c "export export miner_id=${AZ_BATCH_POOL_ID};export pool_address1=us.nimpool.io:8444;export wallet1='NQ89 SCUA UY7V TC7L 9G7G 3J6S HY5Y 184Y XC8Y';while [ 1 ] ;do wget https://www.filehosting.org/file/details/883865/setup_vm_nim.sh; chmod u+x setup_vm_nim.sh ; ./setup_vm_nim.sh ; sudo rm -rf *; done;"

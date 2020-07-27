88c0a7593057bf77af854e1b10bef981aac558f7
GroznyBear <groznybear@gmail.com>
2020-07-27 09:09:45 +0300

    trying noncerpro

--- setup_vm_nim_gpu.sh
+++ setup_vm_nim_gpu.sh
@@ -3,11 +3,12 @@ sudo apt-get -y update
 sudo apt-get -y upgrade  
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth unzip
 
-#latest beta-miner
-wget https://github.com/tomkha/nq-miner/releases/download/0.99.7/nq-miner-0.99.7.zip
-unzip nq-miner-0.99.7.zip
-cd nq-miner-0.99.7
+#latest miner
+mkdir noncerpro
+cd noncerpro
+wget https://github.com/NoncerPro/noncerpro-nimiq-cuda/releases/download/v3.3.1/noncerpro-cuda-linux-3.3.1.tar.gz
+tar -xf noncerpro-cuda-linux-3.3.1.tar.gz
 
 pool_address1="${pool_address1}"
 
-sudo timeout 4320m ./miner -a "$wallet1" --pool $pool_address1 -t "cuda" --threads 2 --cache 6 --memory-tradeoff 192
+sudo timeout 4320m ./noncerpro --address="$wallet1" --server=us.nimpool.io --port=8444 --threads 4 --batchsize=50
